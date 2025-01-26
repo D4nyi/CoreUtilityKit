9ca5a29413e4b7db6b36747d42666f7ff8f81d28
Szöllősi Dániel <szollosi.daniel0@gmail.com>
2025-01-26 16:41:57 +0100

    Added `GetOrAdd` and `TryUpdate` extensions for dictionaries

--- src/CoreUtilityKit/Helpers/ListExtensions.cs
+++ src/CoreUtilityKit/Helpers/ListExtensions.cs
@@ -1,3 +1,6 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
 namespace CoreUtilityKit.Helpers;
 
 /// <summary>
@@ -62,4 +65,121 @@ public static class ListExtensions
     /// <returns><see cref="Dictionary{TKey,TValue}"/> self or an empty <see cref="Dictionary{TKey,TValue}"/></returns>
     public static Dictionary<TKey, TValue> ToEmptyIfNull<TKey, TValue>(this Dictionary<TKey, TValue>? source) where TKey : notnull =>
         source ?? [];
+
+    /// <summary>Adds a key/value pair to the <see cref="Dictionary{TKey,TValue}"/> if the key does not already exist.</summary>
+    /// <param name="dict">The dictionary from which to read a value or add to it.</param>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="value">The value to be added, if the key does not already exist.</param>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <returns>The value for the key. This will be either the existing value for the key if the key is already in the dictionary, or the new value if the key was not in the dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dict"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="key"/> is a null reference.</exception>
+    public static TValue? GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue? value) where TKey : notnull
+    {
+        ArgumentNullException.ThrowIfNull(dict);
+        ArgumentNullException.ThrowIfNull(key);
+
+        ref TValue? val = ref CollectionsMarshal.GetValueRefOrAddDefault(dict, key, out bool exists);
+        if (exists)
+        {
+            return val;
+        }
+
+        val = value;
+        return value;
+    }
+
+    /// <summary>Adds a key/value pair to the <see cref="Dictionary{TKey,TValue}"/> if the key does not already exist.</summary>
+    /// <param name="dict">The dictionary from which to read a value or add to it.</param>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="valueFactory">The function used to generate a value for the key.</param>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <returns>The value for the key. This will be either the existing value for the key if the key is already in the dictionary, or the new value if the key was not in the dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dict"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="key"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="valueFactory"/> is a null reference.</exception>
+    public static TValue? GetOrAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TValue> valueFactory) where TKey : notnull
+    {
+        ArgumentNullException.ThrowIfNull(valueFactory);
+
+        return GetOrAdd(dict, key, valueFactory(key));
+    }
+
+    /// <summary>Adds a key/value pair to the <see cref="Dictionary{TKey,TValue}"/> if the key does not already exist.</summary>
+    /// <param name="dict">The dictionary from which to read a value or add to it.</param>
+    /// <param name="key">The key of the element to add.</param>
+    /// <param name="valueFactory">The function used to generate a value for the key.</param>
+    /// <param name="factoryArgument">An argument value to pass into <paramref name="valueFactory"/>.</param>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <typeparam name="TArg">The type of argument to pass into valueFactory.</typeparam>
+    /// <returns>The value for the key. This will be either the existing value for the key if the key is already in the dictionary, or the new value if the key was not in the dictionary.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dict"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="key"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="valueFactory"/> is a null reference.</exception>
+    public static TValue? GetOrAdd<TKey, TValue, TArg>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TArg, TValue> valueFactory, TArg factoryArgument) where TKey : notnull
+    {
+        ArgumentNullException.ThrowIfNull(valueFactory);
+
+        return GetOrAdd(dict, key, valueFactory(key, factoryArgument));
+    }
+
+    /// <summary>Updates the value associated with <paramref name="key"/> to <paramref name="newValue"/> in the <see cref="Dictionary{TKey,TValue}"/>.</summary>
+    /// <param name="dict">The dictionary in which to update the value by the given key.</param>
+    /// <param name="key">The key whose value will possibly be replaced.</param>
+    /// <param name="newValue">The value that replaces the value of the element with <paramref name="key"/> if the comparison results in equality.</param>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <returns><see langword="true"/> if the value with <paramref name="key"/> replaced with <paramref name="newValue"/>; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dict"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="key"/> is a null reference.</exception>
+    public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue newValue) where TKey : notnull
+    {
+        ref TValue val = ref CollectionsMarshal.GetValueRefOrNullRef(dict, key);
+        if (Unsafe.IsNullRef(ref val))
+        {
+            return false;
+        }
+
+        val = newValue;
+        return true;
+    }
+
+    /// <summary>Updates the value associated with <paramref name="key"/> to a newly generated value by <paramref name="valueFactory"/> in the <see cref="Dictionary{TKey,TValue}"/>.</summary>
+    /// <param name="dict">The dictionary in which to update the value by the given key.</param>
+    /// <param name="key">The key whose value will possibly be replaced.</param>
+    /// <param name="valueFactory">The function used to generate a value for the key.</param>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <returns><see langword="true"/> if the value with <paramref name="key"/> replaced with a new value generated by <paramref name="valueFactory"/>; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dict"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="key"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="valueFactory"/> is a null reference.</exception>
+    public static bool TryUpdate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TValue> valueFactory) where TKey : notnull
+    {
+        ArgumentNullException.ThrowIfNull(valueFactory);
+
+        return TryUpdate(dict, key, valueFactory(key));
+    }
+
+    /// <summary>Updates the value associated with <paramref name="key"/> to a newly generated value by <paramref name="valueFactory"/> in the <see cref="Dictionary{TKey,TValue}"/>.</summary>
+    /// <param name="dict">The dictionary in which to update the value by the given key.</param>
+    /// <param name="key">The key whose value will possibly be replaced.</param>
+    /// <param name="valueFactory">The function used to generate a value for the key.</param>
+    /// <param name="factoryArgument">An argument value to pass into <paramref name="valueFactory"/>.</param>
+    /// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+    /// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+    /// <typeparam name="TArg">The type of argument to pass into valueFactory.</typeparam>
+    /// <returns><see langword="true"/> if the value with <paramref name="key"/> replaced with a new value generated by <paramref name="valueFactory"/>; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="dict"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="key"/> is a null reference.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="valueFactory"/> is a null reference.</exception>
+    public static bool TryUpdate<TKey, TValue, TArg>(this Dictionary<TKey, TValue> dict, TKey key, Func<TKey, TArg, TValue> valueFactory, TArg factoryArgument) where TKey : notnull
+    {
+        ArgumentNullException.ThrowIfNull(valueFactory);
+
+        return TryUpdate(dict, key, valueFactory(key, factoryArgument));
+    }
 }
--- test/CoreUtilityKit.UnitTests/Helpers/ListExtensionTest.cs
+++ test/CoreUtilityKit.UnitTests/Helpers/ListExtensionTest.cs
@@ -218,4 +218,218 @@ public sealed class ListExtensionTest
     }
     #endregion
     #endregion
+
+    #region GetOrAdd
+
+    [Fact]
+    public void GetOrAdd_AddsIfNotExists()
+    {
+        // Arrange
+        Dictionary<int, int> dict = [];
+
+        // Act
+        int addedValue = dict.GetOrAdd(1, 2);
+
+        // Assert
+        addedValue.Should().Be(2);
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(1);
+    }
+
+    [Fact]
+    public void GetOrAdd_GetsIfExists_DoesNotAdd()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        int addedValue = dict.GetOrAdd(2, 4);
+
+        // Assert
+        addedValue.Should().Be(3);
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(2);
+    }
+
+    [Fact]
+    public void GetOrAdd_AddsIfNotExists_ValueFactory()
+    {
+        // Arrange
+        Dictionary<int, int> dict = [];
+
+        // Act
+        int addedValue = dict.GetOrAdd(1, static a => a + 2);
+
+        // Assert
+        addedValue.Should().Be(3);
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(1);
+    }
+
+    [Fact]
+    public void GetOrAdd_GetsIfExists_ValueFactory_DoesNotAdd()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        int addedValue = dict.GetOrAdd(2, static a => a + 2);
+
+        // Assert
+        addedValue.Should().Be(3);
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(2);
+    }
+
+    [Fact]
+    public void GetOrAdd_AddsIfNotExists_ValueFactory_WithArgument()
+    {
+        // Arrange
+        Dictionary<int, int> dict = [];
+
+        // Act
+        int addedValue = dict.GetOrAdd(1, static (a, b) => a + b + 2, 10);
+
+        // Assert
+        addedValue.Should().Be(13);
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(1);
+    }
+
+    [Fact]
+    public void GetOrAdd_GetsIfExists_ValueFactory_WithArgument_DoesNotAdd()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        int addedValue = dict.GetOrAdd(2, static (a, b) => a + b + 2, 10);
+
+        // Assert
+        addedValue.Should().Be(3);
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(2);
+    }
+
+    #endregion
+
+    #region GetOrAdd
+
+    [Fact]
+    public void TryUpdate_Updates_IfExists()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        bool success = dict.TryUpdate(2, 4);
+
+        // Assert
+        success.Should().BeTrue();
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(2);
+        dict.Values.First().Should().Be(4);
+    }
+
+    [Fact]
+    public void TryUpdate_DoesNotUpdates_IfNotExists()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        bool success = dict.TryUpdate(0, 4);
+
+        // Assert
+        success.Should().BeFalse();
+        dict.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void TryUpdate_Updates_IfExists_ValueFactory()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        bool success = dict.TryUpdate(2, static a => a + 2);
+
+        // Assert
+        success.Should().BeTrue();
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(2);
+        dict.Values.First().Should().Be(4);
+    }
+
+    [Fact]
+    public void TryUpdate_DoesNotUpdates_IfNotExists_ValueFactory()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        bool success = dict.TryUpdate(0, static a => a + 2);
+
+        // Assert
+        success.Should().BeFalse();
+        dict.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public void TryUpdate_Updates_IfExists_ValueFactory_WithArgument()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        bool success = dict.TryUpdate(2, static (a, b) => a + b + 2, 10);
+
+        // Assert
+        success.Should().BeTrue();
+        dict.Should().HaveCount(1);
+        dict.Keys.First().Should().Be(2);
+        dict.Values.First().Should().Be(14);
+    }
+
+    [Fact]
+    public void TryUpdate_DoesNotUpdates_IfNotExists_ValueFactory_WithArgument()
+    {
+        // Arrange
+        Dictionary<int, int> dict = new()
+        {
+            { 2, 3 }
+        };
+
+        // Act
+        bool success = dict.TryUpdate(0, static (a, b) => a + b + 2, 10);
+
+        // Assert
+        success.Should().BeFalse();
+        dict.Should().HaveCount(1);
+    }
+    #endregion
 }
