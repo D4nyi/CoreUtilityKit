b59f93849a8c5fa5a9e6f5d0d878fe86709c602a
Dani <szollosi.daniel0@gmail.com>
2026-02-07 11:07:22 +0100

    Update GHA (#1)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CoreUtilityKit
+
+[![Build & Test](https://github.com/D4nyi/CoreUtilityKit/actions/workflows/build.yml/badge.svg)](https://github.com/D4nyi/CoreUtilityKit/actions/workflows/build.yml)
--- src/CoreUtilityKit.EnumAttributeCache/Properties/Assembly.cs
+++ src/CoreUtilityKit.EnumAttributeCache/Properties/Assembly.cs
@@ -1,12 +1,8 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("<Version> Built: <BuildTime>")]
-
 [assembly: AssemblyProduct("CoreUtilityKit.EnumAttributionCache")]
 [assembly: AssemblyTitle("CoreUtilityKit.EnumAttributionCache")]
-[assembly: AssemblyCopyright("Copyright © <ReleaseYear>. Dániel Szöllősi")]
 [assembly: AssemblyDescription("Enum attribution cache for reading and storing attribute values applied on enums.")]
 
 [assembly: CLSCompliant(false)]
